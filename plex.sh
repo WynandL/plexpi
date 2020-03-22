5bc664287c685f8a0cc4055a40492e73ac5dd886
Wynand Lambrechts <wyniemobile@gmail.com>
2020-03-22 16:12:16 +0200

    samba y

--- plex.sh
+++ plex.sh
@@ -9,10 +9,10 @@ sudo apt-get update && sudo apt-get dist-upgrade -y
 sudo apt-get install apt-transport-https
 curl https://downloads.plex.tv/plex-keys/PlexSign.key | sudo apt-key add -
 echo deb https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install plexmediaserver
 
-sudo apt-get install samba samba-common-bin
+sudo apt-get install samba samba-common-bin -y
 sudo echo -e "[myPi]\npath = /mnt/mydisk/Videos\nwriteable=Yes\ncreate mask=0777\ndirectory mask=0777\npublic=yes" | sudo tee -a /etc/samba/smb.conf
 sudo systemctl restart smbd
 
