3493c462a80e4551fc64fd053e7ce38f417189e9
Wynand Lambrechts <wyniemobile@gmail.com>
2020-03-22 16:33:36 +0200

    remove sh

--- plex.sh
+++ plex.sh
@@ -8,11 +8,11 @@ sudo apt-get update && sudo apt-get dist-upgrade -y
 
 sudo apt-get install apt-transport-https
 curl https://downloads.plex.tv/plex-keys/PlexSign.key | sudo apt-key add -
-echo deb https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
+echo deb https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list #yes
 sudo apt-get update -y
 sudo apt-get install plexmediaserver
 
-sudo apt-get install samba samba-common-bin -y
+sudo apt-get install samba samba-common-bin #yes
 sudo echo -e "[myPi]\npath = /mnt/mydisk/Videos\nwriteable=Yes\ncreate mask=0777\ndirectory mask=0777\npublic=yes" | sudo tee -a /etc/samba/smb.conf
 sudo systemctl restart smbd
 
@@ -20,6 +20,7 @@ echo -e "interface eth0\nstatic ip_address=192.168.1.200/24\nstatic routers=192.
 
 sudo mkdir /mnt/mydisk
 echo -e "/dev/sda1 /mnt/mydisk ntfs defaults 0 0" | sudo tee -a /etc/fstab
+sudo rm plex.sh
 sudo reboot
 
 #sudo mount /dev/sda1 /mnt/mydisk
