09f560ebb44ce092fa47ac02ba341d9d49d97931
WynandL <wyniemobile@gmail.com>
2026-01-28 15:28:27 +0200

    Update plex2026_3.sh

--- plex2026_3.sh
+++ plex2026_3.sh
@@ -1,7 +1,20 @@
+###################################
 sudo mkdir -p /mnt/mydisk
+
+###################################
 lsblk -f
+
+###################################
 sudo apt install -y ntfs-3g
+
+###################################
 sudo nano /etc/fstab
-UUID=1234-ABCD  /mnt/mydisk  ntfs  defaults,noatime,nofail  0  0
+#Replace the UUID with the one found after lsblk -f
+UUID=1234-ABCD  /mnt/mydisk  ntfs  defaults,noatime,nofail  0  0 #Ctrl-O-Enter-Ctrl-X
+
+###################################
 sudo mount -a
-sudo reboot
+
+###################################
+#Reboot optional
+#sudo reboot
