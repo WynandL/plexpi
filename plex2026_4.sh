ac414ce168eba1dd763d231420888724bb80330b
WynandL <wyniemobile@gmail.com>
2026-01-28 15:24:48 +0200

    Update plex2026_4.sh

--- plex2026_4.sh
+++ plex2026_4.sh
@@ -1,10 +1,14 @@
-#Copy first three commands, then the next two, and then the last one + reboot (optional)
+#Permissions for Plex + Samba (CRITICAL)
 sudo groupadd media
 sudo usermod -aG media plex
 sudo usermod -aG media pi
 
+###################################
 sudo chown -R pi:media /mnt/mydisk
 sudo chmod -R 775 /mnt/mydisk
 
+###################################
 sudo apt install -y samba
+
+###################################
 sudo reboot
