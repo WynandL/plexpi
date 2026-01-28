c33f7f3b1a3d4ec6c491bb48b26db3a9bcc748c3
WynandL <wyniemobile@gmail.com>
2026-01-28 15:26:40 +0200

    Update plex2026_2.sh

--- plex2026_2.sh
+++ plex2026_2.sh
@@ -1,14 +1,19 @@
-# Install dependencies
+# Install dependencies - entire file can be copied and pasted into CLI
 sudo apt install -y curl gnupg ca-certificates
 
+###################################
 # Add Plex GPG key (modern method)
 curl -fsSL https://downloads.plex.tv/plex-keys/PlexSign.key | \
 sudo gpg --dearmor -o /usr/share/keyrings/plex.gpg
 
+###################################
 # Add Plex repository
 echo "deb [signed-by=/usr/share/keyrings/plex.gpg] https://downloads.plex.tv/repo/deb public main" | \
 sudo tee /etc/apt/sources.list.d/plexmediaserver.list
 
-# Update and install
+###################################
 sudo apt update
 sudo apt install -y plexmediaserver
+
+###################################
+systemctl status plexmediaserver
