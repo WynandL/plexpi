b02894d4334062563060ebf050265625bf809e5d
WynandL <wyniemobile@gmail.com>
2026-01-28 18:09:57 +0200

    Update plex2026_1.sh

--- plex2026_1.sh
+++ plex2026_1.sh
@@ -18,3 +18,21 @@ sudo nmcli con modify "Wired connection 1" \
 
 sudo nmcli con down "Wired connection 1"
 sudo nmcli con up "Wired connection 1"
+
+sudo nmcli con modify "Wired connection 1" \
+  ipv4.method manual \
+  ipv4.addresses 192.168.0.200/24 \
+  ipv4.gateway 192.168.0.1 \
+  ipv4.dns "8.8.8.8,8.8.4.4" \
+  ipv4.ignore-auto-dns yes \
+  ipv4.never-default no \
+  connection.autoconnect yes
+
+
+sudo nmcli con modify "Wired connection 1" ipv4.route-metric 100
+
+sudo nmcli con down "Wired connection 1"
+sudo systemctl restart NetworkManager
+sudo nmcli con up "Wired connection 1"
+
+
