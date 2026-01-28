ae528aae2aef92ea0c1fcb8f30869e6d0b4c5dc6
WynandL <wyniemobile@gmail.com>
2026-01-28 15:29:51 +0200

    Update plex2026_4.sh

--- plex2026_4.sh
+++ plex2026_4.sh
@@ -11,4 +11,20 @@ sudo chmod -R 775 /mnt/mydisk
 sudo apt install -y samba
 
 ###################################
+sudo nano /etc/samba/smb.conf
+
+###################################
+#Copy at the bottom of smb.conf
+[myPi]
+path = /mnt/mydisk/Videos
+browseable = yes
+writeable = yes
+guest ok = yes
+create mask = 0775
+directory mask = 0775
+
+###################################
+sudo systemctl restart smbd
+
+###################################
 sudo reboot
