92e80a83d153b8f6f5fe1916adfc3da342817a54
WynandL <wyniemobile@gmail.com>
2026-01-29 08:08:37 +0200

    Update plex2026_1.sh

--- plex2026_1.sh
+++ plex2026_1.sh
@@ -1,5 +1,5 @@
 #sudo wget https://raw.githubusercontent.com/WynandL/plexpi/master/plex2026_1.sh
-#Enable SSH
+###################################
 sudo systemctl enable ssh
 sudo systemctl start ssh
 systemctl status ssh
@@ -9,30 +9,20 @@ sudo apt update
 sudo apt full-upgrade -y
 sudo reboot
 
-sudo nmcli con modify "Wired connection 1" \
-  ipv4.method manual \
-  ipv4.addresses 192.168.0.200/24 \
-  ipv4.gateway 192.168.0.1 \
-  ipv4.dns "8.8.8.8 8.8.4.4" \
-  ipv4.ignore-auto-dns yes
+###################################
+#Current Pi Network Manager is difficult; keep DHCP for now
+#Plex on TV don't need a static IP (browser will though...)
+#sudo nmcli con modify "Wired connection 1" \
+#  ipv4.method manual \
+#  ipv4.addresses 192.168.0.200/24 \
+#  ipv4.gateway 192.168.0.1 \
+#  ipv4.dns "8.8.8.8,8.8.4.4" \
+#  ipv4.ignore-auto-dns yes \
+#  ipv4.never-default no \
+#  connection.autoconnect yes
 
-sudo nmcli con down "Wired connection 1"
-sudo nmcli con up "Wired connection 1"
+#sudo nmcli con modify "Wired connection 1" ipv4.route-metric 100
 
-sudo nmcli con modify "Wired connection 1" \
-  ipv4.method manual \
-  ipv4.addresses 192.168.0.200/24 \
-  ipv4.gateway 192.168.0.1 \
-  ipv4.dns "8.8.8.8,8.8.4.4" \
-  ipv4.ignore-auto-dns yes \
-  ipv4.never-default no \
-  connection.autoconnect yes
-
-
-sudo nmcli con modify "Wired connection 1" ipv4.route-metric 100
-
-sudo nmcli con down "Wired connection 1"
-sudo systemctl restart NetworkManager
-sudo nmcli con up "Wired connection 1"
-
-
+#sudo nmcli con down "Wired connection 1"
+#sudo systemctl restart NetworkManager
+#sudo nmcli con up "Wired connection 1"
